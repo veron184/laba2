bd6052f332021ed7eae4393db47ea10f68db4eb1
veron184 <peterson07nika@mail.ru>
2024-12-21 23:44:52 +0300

    Create sed_operations.c

---
+++ sed_operations.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <regex.h>
+#include "sed_operations.h"
+
+void replace_text(const char *input_file, const char *old_text, const char *new_text) {
+
+}
+
+void delete_lines(const char *input_file, const char *regex_str) {
+
+}
+
+void insert_text(const char *input_file, const char *insert_text, int at_start) {
+
+}
